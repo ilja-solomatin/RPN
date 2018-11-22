2217b343669dc82e5f9a88afb9a80896a9af6fb6
Ilja Solomatin <ilja.solomatin@mail.ru>
2018-11-22 21:15:31 +0000

    Fully commented the code

--- src/SRPN.java
+++ src/SRPN.java
@@ -1,36 +1,60 @@
 import java.io.*;
 
-public class SRPN {
+/**
+ * Program to replicate the features of the original SRPN program, which is a saturation reverse polish notation
+ * calculator, meaning it will prevent values wrapping around if it is too big or too small to be stored as an integer.
+ * The program also includes some extra features such as using octal numbers, having random numbers using the command "r".
+ */
 
+public class SRPN {
+    //Fields
     private int maxSize = 23;
     private String stack[] = new String[maxSize];
     private int randomNumbers[] = new int[22];
     private int randomNumberTracker = -1;
-    private int pointer = -1;
+    private int stackPointer = -1; //"points" to the top of the stack, -1 at start since nothing is pushed on yet
     private long sum;
     private int operandOne;
     private int operandTwo;
 
+    //Constructor - setting the first value of the stack, and filling the array with 'random' numbers.
     public SRPN() {
         this.stack[0] = "-2147483648";
         this.setRandomNumbers();
     }
 
+    //Methods
+
+    /*
+    This method receives the input that the user gives, and processes the contents. Every If and else if will be used
+    to check what the operator is to perform on the operands. This includes octal numbers starting with "0", =, +, -,
+    *, /, %, ^, d and r.
+     */
     public void processCommand(String input) {
         if(input.startsWith("0")){
+            //If the input string starts with 0, and doesn't have 8's and 9's, then it is an octal number
             if(!input.contains("8") && !input.contains("9")){
-                long octalNumber = Long.parseLong(input, 8);
+                long octalNumber = Long.parseLong(input, 8); //Convert the octal to a decimal number
                 this.push(String.valueOf(octalNumber));
             }
         }
+        //If the user types =, it means the top element on the stack needs to be displayed
         else if(input.charAt(input.length()-1) == '=') {
-            System.out.println(this.peek());
+            if(this.stackPointer >= 0){
+                System.out.println(this.peek());
+            }
+            else{
+                System.out.println("Stack underflow.");
+            }
         }
-        else if(input.charAt(input.length()-1) == '+') {
+        //If the user types +, check if you can remove two items from the stack and proceed to carry out the operation
+        else if(input.charAt(input.length()-1) == '+') { //Addition
             if(canPopTwice()){
                 this.operandTwo = Integer.parseInt(this.pop());
                 this.operandOne = Integer.parseInt(this.pop());
                 this.sum = (long)this.operandOne + (long)this.operandTwo;
+                //Check if the sum is greater than the maximum value an integer can store, in order to prevent
+                //wrapping around
                 if(this.sum > Integer.MAX_VALUE){
                     this.push(String.valueOf(Integer.MAX_VALUE));
                 }
@@ -41,11 +65,12 @@ public class SRPN {
                     this.push(Integer.toString(this.operandOne + this.operandTwo));
                 }
             }
+            //If you cant pop twice, give a relevant error message
             else{
                 System.out.println("Stack underflow.");
             }
         }
-        else if(input.charAt(input.length()-1) == '-'){
+        else if(input.charAt(input.length()-1) == '-'){ //Subtraction
             if(canPopTwice()){
                 this.operandTwo = Integer.parseInt(this.pop());
                 this.operandOne = Integer.parseInt(this.pop());
@@ -64,7 +89,7 @@ public class SRPN {
                 System.out.println("Stack underflow.");
             }
         }
-        else if(input.charAt(input.length()-1) == '*'){
+        else if(input.charAt(input.length()-1) == '*'){ //Multiplication
             if(canPopTwice()){
                 this.operandTwo = Integer.parseInt(this.pop());
                 this.operandOne = Integer.parseInt(this.pop());
@@ -83,14 +108,14 @@ public class SRPN {
                 System.out.println("Stack underflow.");
             }
         }
-        else if(input.charAt(input.length()-1) == '/'){
+        else if(input.charAt(input.length()-1) == '/'){ //Division
             if(canPopTwice()){
                 this.operandTwo = Integer.parseInt(this.pop());
                 this.operandOne = Integer.parseInt(this.pop());
-                if(this.operandTwo != 0){
+                if(this.operandTwo != 0){ //If the denominator is not 0, perform the division
                     this.push(Integer.toString(this.operandOne / this.operandTwo));
                 }
-                else{
+                else{ //Division by 0 error message given, and both operands pushed back on stack
                     System.out.println("Divide by 0.");
                     this.push(Integer.toString(this.operandOne));
                     this.push(Integer.toString(this.operandTwo));
@@ -100,7 +125,7 @@ public class SRPN {
                 System.out.println("Stack underflow.");
             }
         }
-        else if(input.charAt(input.length()-1) == '%'){
+        else if(input.charAt(input.length()-1) == '%'){ //Modulo division
             if(canPopTwice()){
                 this.operandTwo = Integer.parseInt(this.pop());
                 this.operandOne = Integer.parseInt(this.pop());
@@ -110,10 +135,10 @@ public class SRPN {
                 System.out.println("Stack underflow.");
             }
         }
-        else if(input.charAt(input.length()-1) == '^'){
+        else if(input.charAt(input.length()-1) == '^'){ //Exponent
             if(canPopTwice()){
                 this.operandTwo = Integer.parseInt(this.pop());
-                if(this.operandTwo < 0){
+                if(this.operandTwo < 0){ //If a negative power is given, throw an error message and push the power back
                     System.out.println("Negative power.");
                     this.push(Integer.toString(operandTwo));
                 }
@@ -135,21 +160,23 @@ public class SRPN {
                 System.out.println("Stack underflow.");
             }
         }
+        //Display contents of the stack command
         else if(input.contains("d")){
-            for(char letter: input.toCharArray()){
+            for(char letter: input.toCharArray()){ //Iterate through each letter in the given command
                 if(letter == 'd'){
                     this.displayStack();
                 }
-                else{
+                else{ //If the command includes letters that aren't 'd' then display error messages for them
                     System.out.println("Unrecognised operator or operand \"" + letter + "\".");
                 }
             }
         }
+        //Push a random number onto the stack command
         else if(input.contains("r")){
             for(char letter: input.toCharArray()){
-                if(letter == 'r'){
+                if(letter == 'r'){ //If an r is detected, increase r counter by 1, and return a number for the array
                     this.randomNumberTracker++;
-                    if(this.randomNumberTracker > 21){
+                    if(this.randomNumberTracker > 21){ //If the counter is above 21 (out of bounds), put the counter back to 0
                         this.randomNumberTracker = 0;
                     }
                     this.push(Integer.toString(this.getRandomNumber()));
@@ -159,7 +186,9 @@ public class SRPN {
                 }
             }
         }
+        //In this case the inputted command is a regular number and should be pushed onto the stack
         else{
+            //If the values inputted are bigger or smaller than the max/min values, set to the max/min values respectively
             if(Long.parseLong(input) > Integer.MAX_VALUE){
                 this.push(String.valueOf(Integer.MAX_VALUE));
             }
@@ -172,10 +201,11 @@ public class SRPN {
         }
     }
 
+    //Method which will iterate through each element of the stack and print it
     public void displayStack(){
         for(String value: this.stack){
             if(value == null){
-                //Don't print the null value
+                //Don't print the null values
             }
             else{
                 System.out.println(value);
@@ -183,6 +213,7 @@ public class SRPN {
         }
     }
 
+    //Method to set the values of the random array when the object is created
     public void setRandomNumbers(){
         this.randomNumbers[0] = 1804289383;
         this.randomNumbers[1] = 846930886;
@@ -213,61 +244,70 @@ public class SRPN {
     }
 
     public boolean isFull(){
-        return(this.pointer >= this.maxSize -1);
+        return(this.stackPointer >= this.maxSize - 1);
     }
 
+    //Method to place an item onto the stack
     public void push(String operand){
         if(isFull()){
             System.out.println("Stack overflow.");
         }
         else{
-            this.pointer++;
-            this.stack[this.pointer] = operand;
+            this.stackPointer++;
+            this.stack[this.stackPointer] = operand;
         }
     }
 
+    //Method to show the top element of the stack without removing it
     public String peek(){
-        return(this.stack[this.pointer]);
+        return(this.stack[this.stackPointer]);
     }
 
     public boolean canPopTwice(){
-        return(this.pointer >= 1);
+        return(this.stackPointer >= 1);
     }
 
+    //Method to remove the top element from the stack
     public String pop(){
-        String temp = this.stack[pointer];
-        this.stack[pointer] = null;
-        this.pointer--;
-        return temp;
+        String temp = this.stack[stackPointer]; //Store the top element in a temporary value
+        this.stack[stackPointer] = null; //Nullify the element being removed
+        this.stackPointer--;
+        return temp; //Return the element that was removed
     }
 
-    public static void main(String[] args) throws IOException {
-        SRPN srpn = new SRPN();
-        int hashCount = 0;
+    public static void main(String[] args) throws IOException{
+        SRPN srpn = new SRPN(); //Create the new SRPN object
+        int hashCount = 0; //Counter to track amount of "#" entered by the user
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        //Keep accepting inputs
         while(true){
             String command = reader.readLine();
 
-            //Close on an End-of-file (EOF) (Ctrl-D on the terminal)
-            if(command == null) {
-                //Exit code 0 for a graceful exit
+            //Close on an End of file
+            if(command == null){
+                //Exit code 0
                 System.exit(0);
             }
-            for(String commandComponent: command.split(" ")){
+            //Iterate through each argument seperated by spaces " " from the users input
+            for(String commandComponent : command.split(" ")){
                 try{
-                    if(commandComponent.contains("#")){
+                    if(commandComponent.contains("#")){ //A comment has started
                         hashCount++;
                     }
+                    //If there is an even number of hashes, then commenting has finished and subsequent arguments
+                    //can be processed
                     if(hashCount % 2 == 0){
                         if(commandComponent.isEmpty() || commandComponent.equals("#")){
                             //Do nothing as the user has pressed the enter key
                         }
                         else{
-                            srpn.processCommand(commandComponent);
+                            srpn.processCommand(commandComponent); //Send the input to be processed
                         }
                     }
                 } catch(Exception e){
+                    //When there is an error from the input from the user, an error message is given to each
+                    //individual character
                     for(char letters: commandComponent.toCharArray()){
                         System.out.println("Unrecognised operator or operand \"" + letters + "\".");
                     }
